92bde8a4888a7fe50be27d735e0ce8d6728e1119
rhv9 <rahulhasmukv@gmail.com>
2024-02-23 18:05:09 +0000

    Removed discovery panel

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -12,12 +12,10 @@ namespace JobListTracker.MVVM.ViewModel
     class MainViewModel : ObservableObject
     {
         public RelayCommand HomeViewCommand { get; set; }
-        public RelayCommand DiscoveryViewCommand { get; set; }
         public RelayCommand JobListViewCommand { get; set; }
         public RelayCommand AddUserViewCommand { get; set; }
 
         public HomeViewModel HomeVm { get; set; }
-        public DiscoveryViewModel DiscoveryVm { get; set; }
         public JobListViewModel JobListVm { get; set; }
         public AddUserViewModel AddUserVm { get; set; }
 
@@ -37,7 +35,6 @@ namespace JobListTracker.MVVM.ViewModel
             JobApplicationManager.Init();
 
             HomeVm = new HomeViewModel();
-            DiscoveryVm = new DiscoveryViewModel();
             JobListVm = new JobListViewModel();
             AddUserVm = new AddUserViewModel();
 
@@ -46,11 +43,6 @@ namespace JobListTracker.MVVM.ViewModel
             HomeViewCommand = new RelayCommand(o =>
             {
                 CurrentView = HomeVm;
-            });
-
-            DiscoveryViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = DiscoveryVm;
             });
 
             JobListViewCommand = new RelayCommand(o =>
